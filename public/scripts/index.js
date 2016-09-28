1a77ef25244ebcb89fdb26e91d6869b00d27879b
Jason Boxman <jasonb@edseek.com>
2016-09-28 00:05:50 -0400

    Add live demo.

---
+++ public/scripts/index.js
@@ -0,0 +1,18 @@
+$(function() {
+  $('#short-btn').click(function() {
+    $.ajax({
+      type: 'post',
+      url: '/api/save',
+      data: JSON.stringify({
+        url: $('#url').val()
+      }),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: function(payload) {
+        $('#shorturl').val(payload.shortUrl);
+      },
+      // Bad news
+      failure: function() {}
+    })
+  });
+});
